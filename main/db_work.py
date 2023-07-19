b204d6392b6c97ed2f6ef509b511b82e60b02628
Solomkin Alexandr <solomin1995@mail.ru>
2023-07-20 02:14:41 +0300

    daily_planner v0.3: implementation of the function for adding new notes

--- main/db_work.py
+++ main/db_work.py
@@ -1,5 +1,6 @@
 from os import path
 import csv
+import datetime
 
 all_data = {}
 last_id = 0
@@ -17,9 +18,41 @@ def read_all():
             last_id = all_data[-1]["id"]
             return all_data
     else:
-        print("The database is not connected!")
+        print("База данных не подключена!")
 
 
 def print_all():
     for_output = [" ".join(k.values()) for k in all_data]
     print(*for_output, sep="\n", end=f"\n{'-' * 20}\n\n")
+
+
+def add_entry(data):
+    global last_id
+
+    if all(data.values()) and matching_rec(data, all_data):
+        last_id = int(last_id) + 1
+        data["id"] = last_id
+
+        with open(name_db, "a", encoding="utf-8", newline="") as file:
+            fieldnames = ["id", "заголовок", "тело заметки", "дата последней редакции"]
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writerow(data)
+            print("Заметка добавлена")
+    else:
+        print("В базе данных уже существует данная заметка")
+
+
+def matching_rec(new_entry: dict, all_info):
+    value = list(new_entry.values())[1:]
+    all_values = [list(k.values())[1:] for k in all_info]
+    return value not in all_values
+
+
+def check_new_data(num):
+    answer = input(f"Введите {num}: ")
+    while True:
+        if num in "заголовок тело заметки":
+            if answer.isalpha():
+                break
+        
+    return answer
--- main/user_interface.py
+++ main/user_interface.py
@@ -1,4 +1,5 @@
 from db_work import *
+import datetime
 
 def menu():
     while True:
@@ -12,3 +13,15 @@ def menu():
         match actions:
             case "1":
                 print_all()
+            case "3":
+                add_entry(add_menu())
+
+
+def add_menu():
+    add_dict = {"id": "1", "заголовок": "", "тело заметки": "", "дата последней редакции": ""}
+    for i in add_dict:
+        if i != "id" and i != "дата последней редакции":
+            add_dict[i] = check_new_data(i)
+    date = datetime.datetime.now()
+    add_dict[i] = date.strftime("%d-%m-%Y %H:%M")
+    return add_dict
